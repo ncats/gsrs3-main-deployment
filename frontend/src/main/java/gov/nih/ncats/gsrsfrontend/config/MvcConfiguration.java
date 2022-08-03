cc9b3fbae281c854e6a4b0215e25526e4b8c33e7
epuzanov <epuzanov@users.noreply.github.com>
2022-08-03 23:20:57 +0200

    Use the local directory specified in the gsrs.frontend.config.dir option to store customized configuration and images files

--- frontend/src/main/java/gov/nih/ncats/gsrsfrontend/config/MvcConfiguration.java
+++ frontend/src/main/java/gov/nih/ncats/gsrsfrontend/config/MvcConfiguration.java
@@ -1,7 +1,13 @@
 package gov.nih.ncats.gsrsfrontend.config;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.PathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,30 +16,42 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.beans.factory.annotation.Value;
 
-import java.io.IOException;
-
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
     @Value("${route.prefix:ginas/app/beta/}")
     private String prefix = "ginas/app/beta/";
-    
 
-    //This is so all the front end refresh/ non-existing files default back to index.html
+    @Value("${gsrs.frontend.config.dir:classpath:/static/assets/data}")
+    private String frontendConfigDir = "classpath:/static/assets/data";
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-           registry.addResourceHandler("/**")
+        registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver() {
                     @Override
                     protected Resource getResource(String resourcePath, Resource location) throws IOException {
                         if(resourcePath.startsWith(prefix)){
-                               resourcePath = resourcePath.substring(prefix.length());
+                            resourcePath = resourcePath.substring(prefix.length());
+                        }
+                        Resource indexPage = new ClassPathResource("/static/index.html");
+                        if (!frontendConfigDir.startsWith("classpath:/")) {
+                            Path fp = Paths.get(frontendConfigDir + "/index.html");
+                            if (Files.isReadable(fp)) {
+                                indexPage = new PathResource(fp);
+                            }
+                            if (resourcePath.startsWith("assets/data/")
+                                    || resourcePath.startsWith("assets/images/")
+                                    || resourcePath.endsWith("styles.custom.css")) {
+                                fp = Paths.get(frontendConfigDir + "/" + resourcePath.substring(resourcePath.lastIndexOf("/")+1));
+                                if (Files.isReadable(fp)) {
+                                    return new PathResource(fp);
+                                }
+                            }
                         }
                         Resource requestedResource = location.createRelative(resourcePath);
-                        
-                        return (requestedResource.exists() && requestedResource.isReadable()) ? requestedResource
-                                : new ClassPathResource("/static/index.html");
+                        return (requestedResource.exists() && requestedResource.isReadable()) ? requestedResource : indexPage;
                     }
                 });
     }
