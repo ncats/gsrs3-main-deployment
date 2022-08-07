bd12f5c592250ad541fe9b1b1d36c10515550e0f
epuzanov <epuzanov@users.noreply.github.com>
2022-08-07 19:53:02 +0200

    fix gateway package

--- gateway/src/main/java/gsrs/ncats/gateway/config/MvcConfiguration.java
+++ gateway/src/main/java/gsrs/ncats/gateway/config/MvcConfiguration.java
@@ -1,4 +1,4 @@
-package gov.nih.ncats.adverseevents.config;
+package gsrs.ncats.gateway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
