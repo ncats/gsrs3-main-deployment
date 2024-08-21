2b612cde8f1d0ee2873f4dd9bf77b212bc83f986
Mitch Miller <mitch.miller@thinkscience.us>
2024-08-21 14:11:20 -0400

    added required JAR files to extraJars dir under substances, added scripts to install these JAR files modified application POM to add 2 missing dependencies

---
+++ substances/installExtraJars.cmd
@@ -0,0 +1,7 @@
+mvnw.cmd install:install-file -Dfile=extraJars/applications-api-3.1-SNAPSHOT.jar
+mvnw.cmd install:install-file -Dfile=extraJars/gsrs-spring-boot-starter-3.1-SNAPSHOT.jar
+mvnw.cmd install:install-file -Dfile=extraJars/gsrs-spring-legacy-indexer-3.1-SNAPSHOT.jar
+mvnw.cmd install:install-file -Dfile=extraJars/gsrs-core-entities-3.1-SNAPSHOT.jar
+mvnw.cmd install:install-file -Dfile=extraJars/gsrs-scheduled-tasks-3.1-SNAPSHOT.jar
+mvnw.cmd install:install-file -Dfile=extraJars/gsrs-spring-akka-3.1-SNAPSHOT.jar
+mvnw.cmd install:install-file -Dfile=extraJars/akka-actor_2.11-2.5.32.jar  -DgroupId=com.typesafe.akka -DartifactId=akka-actor_2.11 -Dversion=2.5.32 -Dpackaging=JAR
