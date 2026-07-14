6c30f461acab1af1bb3c5fdd27ec179d54d381eb
alx652 <welscha2@nih.gov>
2026-07-14 18:06:22 -0400

    closer to release

--- substances/installExtraJars.sh
+++ substances/installExtraJars.sh
@@ -1,4 +1,4 @@
-./mvnw install:install-file -Dfile=extraJars/Featureize-Nitrosamines-0.0.4-SNAPSHOT.jar
-./mvnw install:install-file -Dfile=extraJars/applications-api-3.2.0-SNAPSHOT.jar
-./mvnw install:install-file -Dfile=extraJars/products-api-3.2.0-SNAPSHOT.jar
-./mvnw install:install-file -Dfile=extraJars/clinical-trials-api-3.2.0-SNAPSHOT.jar
+./mvnw install:install-file -Dfile=extraJars/Featureize-Nitrosamines-0.0.4.jar
+./mvnw install:install-file -Dfile=extraJars/applications-api-3.2.0.jar
+./mvnw install:install-file -Dfile=extraJars/products-api-3.2.0.jar
+./mvnw install:install-file -Dfile=extraJars/clinical-trials-api-3.2.0.jar
